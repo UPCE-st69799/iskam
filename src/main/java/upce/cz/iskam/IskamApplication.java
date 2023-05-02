fec543f3cfd853074faf441a4feb5a316c07953d
Jan Korbel (st69799) <71786085+duffi4545@users.noreply.github.com>
2023-05-02 21:14:28 +0200

    initial

---
+++ src/main/java/upce/cz/iskam/IskamApplication.java
@@ -0,0 +1,13 @@
+package upce.cz.iskam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IskamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IskamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/upce/cz/iskam/IskamApplicationTests.java
@@ -0,0 +1,13 @@
+package upce.cz.iskam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IskamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
